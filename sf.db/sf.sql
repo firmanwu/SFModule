fa0ea2b33e8ce2579d6caba3499fc5e1f45a3a34
William Wu <william.wu@g-byte.com>
2018-06-09 08:35:09 +0800

    Update database file.

--- sf.db/sf.sql
+++ sf.db/sf.sql
@@ -27,18 +27,15 @@ CREATE TABLE IF NOT EXISTS `finishedgood` (
   PRIMARY KEY (`finishedGoodID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在傾印表格  sf.finishedgood 的資料：~0 rows (大約)
-/*!40000 ALTER TABLE `finishedgood` DISABLE KEYS */;
-/*!40000 ALTER TABLE `finishedgood` ENABLE KEYS */;
-
+-- 取消選取資料匯出。
 -- 傾印  表格 sf.finishedgoodentry 結構
 CREATE TABLE IF NOT EXISTS `finishedgoodentry` (
   `finishedGoodEntryID` varchar(256) NOT NULL,
   `serialNumber` varchar(256) NOT NULL,
   `status` varchar(256) NOT NULL,
   `storedArea` varchar(8) NOT NULL,
-  `product` varchar(256) NOT NULL,
   `batchNumber` varchar(256) NOT NULL,
+  `product` varchar(256) NOT NULL,
   `storedDate` date NOT NULL,
   `storedPackageNumber` int(16) NOT NULL,
   `palletNumber` int(16) NOT NULL,
@@ -48,10 +45,7 @@ CREATE TABLE IF NOT EXISTS `finishedgoodentry` (
   CONSTRAINT `entryFinishedGood` FOREIGN KEY (`product`) REFERENCES `finishedgood` (`finishedGoodID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在傾印表格  sf.finishedgoodentry 的資料：~0 rows (大約)
-/*!40000 ALTER TABLE `finishedgoodentry` DISABLE KEYS */;
-/*!40000 ALTER TABLE `finishedgoodentry` ENABLE KEYS */;
-
+-- 取消選取資料匯出。
 -- 傾印  表格 sf.finishedgoodrequisition 結構
 CREATE TABLE IF NOT EXISTS `finishedgoodrequisition` (
   `finishedGoodRequistionID` varchar(256) NOT NULL,
@@ -69,10 +63,7 @@ CREATE TABLE IF NOT EXISTS `finishedgoodrequisition` (
   CONSTRAINT `requisitionProduct` FOREIGN KEY (`product`) REFERENCES `finishedgood` (`finishedGoodID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在傾印表格  sf.finishedgoodrequisition 的資料：~0 rows (大約)
-/*!40000 ALTER TABLE `finishedgoodrequisition` DISABLE KEYS */;
-/*!40000 ALTER TABLE `finishedgoodrequisition` ENABLE KEYS */;
-
+-- 取消選取資料匯出。
 -- 傾印  表格 sf.material 結構
 CREATE TABLE IF NOT EXISTS `material` (
   `materialID` varchar(256) NOT NULL,
@@ -83,17 +74,7 @@ CREATE TABLE IF NOT EXISTS `material` (
   PRIMARY KEY (`materialID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在傾印表格  sf.material 的資料：~6 rows (大約)
-/*!40000 ALTER TABLE `material` DISABLE KEYS */;
-REPLACE INTO `material` (`materialID`, `materialName`, `totalPackageNumber`, `totalWeight`, `totalMoney`) VALUES
-	('013A', '精鹽', 0, 0, 0),
-	('113', '尿素膠', 10, 10000, 160000),
-	('217', '鋁粉', 0, 0, 0),
-	('219A', '二甲苯', 30, 5160, 134160),
-	('435', '土狀黑鉛', 0, 0, 0),
-	('439A', '氧化鐵', 0, 0, 0);
-/*!40000 ALTER TABLE `material` ENABLE KEYS */;
-
+-- 取消選取資料匯出。
 -- 傾印  表格 sf.materialentry 結構
 CREATE TABLE IF NOT EXISTS `materialentry` (
   `materialEntryID` varchar(256) NOT NULL,
@@ -101,7 +82,6 @@ CREATE TABLE IF NOT EXISTS `materialentry` (
   `purchaseOrder` varchar(256) NOT NULL,
   `QRCode` blob DEFAULT NULL,
   `storedArea` varchar(8) NOT NULL,
-  `batchNumber` varchar(256) DEFAULT NULL,
   `storedDate` date NOT NULL,
   `packageNumberOfPallet` int(16) NOT NULL,
   `palletNumber` int(16) NOT NULL,
@@ -114,15 +94,7 @@ CREATE TABLE IF NOT EXISTS `materialentry` (
   CONSTRAINT `entryPurchaseOrder` FOREIGN KEY (`purchaseOrder`) REFERENCES `purchaseorder` (`purchaseOrderID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在傾印表格  sf.materialentry 的資料：~1 rows (大約)
-/*!40000 ALTER TABLE `materialentry` DISABLE KEYS */;
-REPLACE INTO `materialentry` (`materialEntryID`, `serialNumber`, `purchaseOrder`, `QRCode`, `storedArea`, `batchNumber`, `storedDate`, `packageNumberOfPallet`, `palletNumber`, `storedPackageNumber`, `storedWeight`, `storedMoney`, `confirmation`) VALUES
-	('EN001', '20180603001', 'PO001', NULL, 'A', '20180506001', '2018-06-03', 5, 2, 10, 10000, 160000, 1),
-	('EN002', '20180603002', 'PO002', NULL, 'B', '20180506002', '2018-06-03', 10, 3, 30, 5160, 134160, 1),
-	('EN003', '20180603003', 'PO004', NULL, 'E', '20180506003', '2018-06-03', 5, 4, 20, 16000, 688000, 0),
-	('EN004', '20180603004', 'PO003', NULL, 'C', '20180506004', '2018-06-03', 2, 3, 6, 6000, 84000, 0);
-/*!40000 ALTER TABLE `materialentry` ENABLE KEYS */;
-
+-- 取消選取資料匯出。
 -- 傾印  表格 sf.materialrequisition 結構
 CREATE TABLE IF NOT EXISTS `materialrequisition` (
   `materialRequisitionID` varchar(256) NOT NULL,
@@ -142,10 +114,7 @@ CREATE TABLE IF NOT EXISTS `materialrequisition` (
   CONSTRAINT `requisitionSupplierID` FOREIGN KEY (`supplier`) REFERENCES `supplier` (`supplierID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在傾印表格  sf.materialrequisition 的資料：~0 rows (大約)
-/*!40000 ALTER TABLE `materialrequisition` DISABLE KEYS */;
-/*!40000 ALTER TABLE `materialrequisition` ENABLE KEYS */;
-
+-- 取消選取資料匯出。
 -- 傾印  表格 sf.materialusage 結構
 CREATE TABLE IF NOT EXISTS `materialusage` (
   `materialUsageID` int(16) unsigned NOT NULL AUTO_INCREMENT,
@@ -156,18 +125,7 @@ CREATE TABLE IF NOT EXISTS `materialusage` (
   CONSTRAINT `materialUsageMaterialID` FOREIGN KEY (`material`) REFERENCES `material` (`materialID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
--- 正在傾印表格  sf.materialusage 的資料：~7 rows (大約)
-/*!40000 ALTER TABLE `materialusage` DISABLE KEYS */;
-REPLACE INTO `materialusage` (`materialUsageID`, `material`, `usingDepartment`) VALUES
-	(1, '013A', '混合組'),
-	(2, '113', '樹脂區'),
-	(3, '217', '混合組'),
-	(4, '219A', '塗模劑'),
-	(5, '435', '塗模劑'),
-	(6, '439A', '混合組'),
-	(7, '439A', '塗模劑');
-/*!40000 ALTER TABLE `materialusage` ENABLE KEYS */;
-
+-- 取消選取資料匯出。
 -- 傾印  表格 sf.packaging 結構
 CREATE TABLE IF NOT EXISTS `packaging` (
   `packagingID` int(16) unsigned NOT NULL AUTO_INCREMENT,
@@ -179,18 +137,7 @@ CREATE TABLE IF NOT EXISTS `packaging` (
   CONSTRAINT `packagingMaterialID` FOREIGN KEY (`material`) REFERENCES `material` (`materialID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
--- 正在傾印表格  sf.packaging 的資料：~7 rows (大約)
-/*!40000 ALTER TABLE `packaging` DISABLE KEYS */;
-REPLACE INTO `packaging` (`packagingID`, `material`, `packaging`, `unitWeight`) VALUES
-	(1, '013A', '包', 25),
-	(2, '217', '噸袋', 800),
-	(3, '219A', '桶', 172),
-	(4, '435', '噸袋', 1000),
-	(5, '439A', '噸袋', 850),
-	(6, '113', '噸桶', 1000),
-	(7, '113', '桶', 240);
-/*!40000 ALTER TABLE `packaging` ENABLE KEYS */;
-
+-- 取消選取資料匯出。
 -- 傾印  表格 sf.purchaseorder 結構
 CREATE TABLE IF NOT EXISTS `purchaseorder` (
   `purchaseOrderID` varchar(256) NOT NULL,
@@ -209,15 +156,7 @@ CREATE TABLE IF NOT EXISTS `purchaseorder` (
   CONSTRAINT `purchaseOrderSupplierID` FOREIGN KEY (`supplier`) REFERENCES `supplier` (`supplierID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在傾印表格  sf.purchaseorder 的資料：~3 rows (大約)
-/*!40000 ALTER TABLE `purchaseorder` DISABLE KEYS */;
-REPLACE INTO `purchaseorder` (`purchaseOrderID`, `material`, `supplier`, `packaging`, `purchaseCondition`, `purchasedPackageNumber`, `notEnteredPackageNumber`) VALUES
-	('PO001', '113', 11, 6, '一般', 30, 20),
-	('PO002', '219A', 5, 3, '一般', 50, 20),
-	('PO003', '435', 7, 4, '特採', 10, 4),
-	('PO004', '217', 3, 2, '回收料', 20, 0);
-/*!40000 ALTER TABLE `purchaseorder` ENABLE KEYS */;
-
+-- 取消選取資料匯出。
 -- 傾印  表格 sf.supplier 結構
 CREATE TABLE IF NOT EXISTS `supplier` (
   `supplierID` int(16) unsigned NOT NULL AUTO_INCREMENT,
@@ -229,23 +168,7 @@ CREATE TABLE IF NOT EXISTS `supplier` (
   CONSTRAINT `supplierMaterial` FOREIGN KEY (`material`) REFERENCES `material` (`materialID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
--- 正在傾印表格  sf.supplier 的資料：~10 rows (大約)
-/*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
-REPLACE INTO `supplier` (`supplierID`, `supplierName`, `material`, `unitPrice`) VALUES
-	(1, '建鹽', '013A', '4'),
-	(2, '冠佳', '013A', '4'),
-	(3, '弘竹', '217', '43'),
-	(4, '鮮豐', '217', '43'),
-	(5, '興龍銓', '219A', '26'),
-	(6, '南方石墨', '435', '14'),
-	(7, '潤優', '435', '14'),
-	(8, '青島泛化', '435', '14'),
-	(9, '晟邦', '435', '14'),
-	(10, '穗曄', '439A', '16'),
-	(11, '佳欣', '113', '16'),
-	(12, ' 欣和', '113', '16');
-/*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
-
+-- 取消選取資料匯出。
 -- 傾印  表格 sf.user 結構
 CREATE TABLE IF NOT EXISTS `user` (
   `userID` varchar(256) NOT NULL,
@@ -255,12 +178,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在傾印表格  sf.user 的資料：~1 rows (大約)
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-REPLACE INTO `user` (`userID`, `userName`, `password`, `authority`) VALUES
-	('admin', 'System Administrator', '$2y$10$VIBa5Ziq56jFt5HjI83EKOzG7618HQ9vVp7N3cSB.HJiNMvcCs2ay', 'admin');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-
+-- 取消選取資料匯出。
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
