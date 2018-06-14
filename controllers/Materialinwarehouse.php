2db2920c8cf988447cb8ed5192b2cec8e55ce267
William Wu <william.wu@g-byte.com>
2018-06-14 19:19:50 +0800

    Add more controller interface of Materialinwarehouse for Materialrequisition.

---
+++ controllers/Materialinwarehouse.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Materialinwarehouse extends CI_Controller {
+
+    public function queryMaterialNameIDInWarehouse(){
+        $this->load->model('materialinwarehousemodel');
+
+        $query = $this->materialinwarehousemodel->queryMaterialNameIDInWarehouseData();
+        echo json_encode($query->result_array());
+    }
+
+    public function querySupplierNameIDInWareHouseByMaterialID($materialID){
+        $this->load->model('materialinwarehousemodel');
+
+        $query = $this->materialinwarehousemodel->querySupplierNameIDInWareHouseByMaterialIDData($materialID);
+        echo json_encode($query->result_array());
+    }
+
+    public function queryPackagingNameIDInWareHouseByMaterialSupplierID($materialID, $supplierID){
+        $this->load->model('materialinwarehousemodel');
+
+        $query = $this->materialinwarehousemodel->queryPackagingNameIDInWareHouseByMaterialSupplierIDData($materialID, $supplierID);
+        echo json_encode($query->result_array());
+    }
+
+    public function queryMaterialInWarehouseDataByMaterialSupplierPackagingID($materialID, $supplierID, $packagingID){
+        $this->load->model('materialinwarehousemodel');
+
+        $query = $this->materialinwarehousemodel->queryMaterialInWarehouseDataByMaterialSupplierPackagingIDData($materialID, $supplierID, $packagingID);
+        echo json_encode($query->result_array());
+    }
+}
--- models/Materialinwarehousemodel.php
+++ models/Materialinwarehousemodel.php
@@ -7,7 +7,8 @@ class Materialinwarehousemodel extends CI_Model {
     {
         $materialInWarehouseData['material'] = $materialEntryData['material'];
         $materialInWarehouseData['materialEntry'] = $materialEntryData['materialEntryID'];
-        $materialInWarehouseData['packaging'] = $materialEntryData['packagingID'];
+        $materialInWarehouseData['supplier'] = $materialEntryData['supplier'];
+        $materialInWarehouseData['packagingID'] = $materialEntryData['packagingID'];
         $materialInWarehouseData['storedArea'] = $materialEntryData['expectedStoredArea'];
         // For Taiwan GMT+8
         $currentDateTime = gmdate("Y-m-d H:i:s", (time() + (28800)));
@@ -18,5 +19,103 @@ class Materialinwarehousemodel extends CI_Model {
         $result = $this->db->insert('materialinwarehouse', $materialInWarehouseData);
 
         return $materialInWarehouseData;
+    }
+
+    public function queryMaterialNameIDInWarehouseData()
+    {
+        $this->db->select('
+            materialinwarehouse.material,
+            material.materialName');
+        $this->db->distinct();
+        $this->db->from('materialinwarehouse');
+        $this->db->join('material', 'materialinwarehouse.material = material.materialID');
+        $result = $this->db->get();
+
+        return $result;
+    }
+
+    public function querySupplierNameIDInWareHouseByMaterialIDData($materialID)
+    {
+        $this->db->select('
+            materialinwarehouse.supplier,
+            supplier.supplierName');
+        $this->db->distinct();
+        $this->db->from('materialinwarehouse');
+        $this->db->join('supplier', 'materialinwarehouse.supplier = supplier.supplierID');
+        $this->db->where('materialinwarehouse.material', $materialID);
+        $result = $this->db->get();
+
+        return $result;
+    }
+
+    public function queryPackagingNameIDInWareHouseByMaterialSupplierIDData($materialID, $supplierID)
+    {
+        $this->db->select('
+            materialinwarehouse.packagingID,
+            packaging.packaging');
+        $this->db->distinct();
+        $this->db->from('materialinwarehouse');
+        $this->db->join('packaging', 'materialinwarehouse.packagingID = packaging.packagingID');
+        $this->db->where('materialinwarehouse.material', $materialID);
+        $this->db->where('materialinwarehouse.supplier', $supplierID);
+        $result = $this->db->get();
+
+        return $result;
+    }
+
+    public function queryMaterialInWarehouseDataByMaterialSupplierPackagingIDData(
+        $materialID,
+        $supplierID,
+        $packagingID)
+    {
+        $this->db->select('
+            materialinwarehouse.materialEntry,
+            material.materialName,
+            supplier.supplierName,
+            packaging.packaging,
+            materialinwarehouse.storedArea,
+            materialinwarehouse.storedPackageNumber');
+        $this->db->from('materialinwarehouse');
+        $this->db->join('material', 'materialinwarehouse.material = material.materialID');
+        $this->db->join('supplier', 'materialinwarehouse.supplier = supplier.supplierID');
+        $this->db->join('packaging', 'materialinwarehouse.packagingID = packaging.packagingID');
+        $this->db->where('materialinwarehouse.material', $materialID);
+        $this->db->where('materialinwarehouse.supplier', $supplierID);
+        $this->db->where('materialinwarehouse.packagingID', $packagingID);
+        $result = $this->db->get();
+
+        return $result;
+    }
+
+    public function queryMaterialInWareHouseStoredPackageNumberWeightMoney(
+        $material,
+        $supplier,
+        $packaging)
+    {
+        $this->db->select('storedPackageNumber, storedWeight, storedMoney');
+        $this->db->from('materialinwarehouse');
+        $this->db->where('material', $material);
+        $this->db->where('supplier', $supplier);
+        $this->db->where('packagingID', $packaging);
+        $result = $this->db->get();
+
+        return $result->row_array();
+    }
+
+    public function updateMaterialInWareHouseQuantityData(
+        $material,
+        $supplier,
+        $packaging,
+        $packageNumber,
+        $weight,
+        $money)
+    {
+        $this->db->set('storedPackageNumber', 'storedPackageNumber + ' . $packageNumber, FALSE);
+        $this->db->set('storedWeight', 'storedWeight + ' . $weight, FALSE);
+        $this->db->set('storedMoney', 'storedMoney + ' . $money, FALSE);
+        $this->db->where('material', $material);
+        $this->db->where('supplier', $supplier);
+        $this->db->where('packagingID', $packaging);
+        $result = $this->db->update('materialinwarehouse');
     }
 }
