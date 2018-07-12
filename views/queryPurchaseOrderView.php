755fce88c19df2e5f0739e74967f9e08daa70ef8
William Wu <william.wu@g-byte.com>
2018-07-12 13:49:32 +0800

    Revise the listed items.

--- views/queryPurchaseOrderView.php
+++ views/queryPurchaseOrderView.php
@@ -18,8 +18,7 @@ function queryPurchaseOrder() {
         success: function(result) {
             $('#queryPurchaseOrderTable').remove();
             var row = JSON.parse(result);
-            //var header = ["採購單編號", "原料", "進貨條件", "刪除];
-            var header = ["採購單編號", "原料", "供應商", "單價", "包裝", "單位重量", "進貨條件", "採購數量", "未入料數量"];
+            var header = ["採購單編號", "原料編號", "原料", "供應商", "單價", "包裝", "單位重量", "進貨條件", "採購數量", "未入料數量"];
             var table = $(document.createElement('table'));
             table.attr('id', 'queryPurchaseOrderTable');
             table.appendTo($('#purchaseOrderList'));
