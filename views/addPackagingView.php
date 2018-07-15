7b6a250aa90f508649c9f582905fe1699df00f88
William Wu <william.wu@g-byte.com>
2018-07-15 14:04:50 +0800

    Add displaying added packaging list and reset button.

--- views/addPackagingView.php
+++ views/addPackagingView.php
@@ -4,27 +4,72 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <script>
 $(document).ready(function() {
-    $.ajax({
-        url: "/material/queryMaterialNameWithID",
-        success: function(result) {
-            var row = JSON.parse(result);
+    function autoFillMaterial() {
+        $.ajax({
+            url: "/material/queryMaterialNameWithID",
+            success: function(result) {
+                var row = JSON.parse(result);
 
-            for(var i in row)
-            {
-                selectOption = $(document.createElement('option'));
-                for(var j in row[i])
+                for(var i in row)
                 {
-                    if ("materialID" == j) {
-                        var materialID = row[i][j];
-                        selectOption.attr('value', row[i][j]);
+                    selectOption = $(document.createElement('option'));
+                    for(var j in row[i])
+                    {
+                        if ("materialID" == j) {
+                            var materialID = row[i][j];
+                            selectOption.attr('value', row[i][j]);
+                        }
+                        var listedName = row[i][j] + "[" + materialID + "]";
+                        if ("materialName" == j) {
+                            selectOption.text(listedName);
+                        }
                     }
-                    var listedName = row[i][j] + "[" + materialID + "]";
-                    if ("materialName" == j) {
-                        selectOption.text(listedName);
+                    selectOption.appendTo($('#materialInPackaging'));
+                }
+            }
+        });
+    }
+    autoFillMaterial();
+
+    // Display added packaging information when material selected
+    $('#materialInPackagingSelection').on("change", '#materialInPackaging', function() {
+        var materialID = $('select#materialInPackaging').find("option:selected").val();
+
+        if ("請選擇" != materialID) {
+            $.ajax({
+                url: "/packaging/queryPackagingbyMaterialID/" + materialID,
+                success: function(result) {
+                    $('#addedPackagingTable').remove();
+                    var row = JSON.parse(result);
+                    var header = ["已新增的包裝"];
+                    var table = $(document.createElement('table'));
+                    table.attr('id', 'addedPackagingTable');
+                    table.appendTo($('#addedPackagingList'));
+                    var tr = $(document.createElement('tr'));
+                    tr.appendTo(table);
+                    for(var i in header)
+                    {
+                        var th = $(document.createElement('th'));
+                        th.text(header[i]);
+                        th.appendTo(tr);
+                    }
+
+                    for(var j in row)
+                    {
+                        tr = $(document.createElement('tr'));
+                        tr.appendTo(table);
+                        for(var k in row[j])
+                        {
+                            if ("packagingID" == k) {
+                                continue;
+                            }
+                            var td = $(document.createElement('td'));
+                            td.text(row[j][k]);
+                            td.appendTo(tr);
+                        }
                     }
                 }
-                selectOption.appendTo($('#materialInPackaging'));
-            }
+            });
         }
     });
 
@@ -63,6 +108,22 @@ $(document).ready(function() {
         });
         event.preventDefault();
     });
+
+    // When click reset button
+    $('input[type="reset"]').click(function() {
+        // Remove options of material then create again
+        $('select#materialInPackaging option').each( function() {
+            if ("請選擇" != $(this).text()) {
+                $(this).remove();
+            }
+        });
+        autoFillMaterial();
+
+        // Remove added packaging information table
+        $('#addedPackagingTable').remove();
+        // Remove current adding packaging information table
+        $('#addPackagingTable').remove();
+    });
 });
 </script>
 
@@ -77,17 +138,19 @@ $(document).ready(function() {
     <div data-role="controlgroup" data-type="horizontal" data-theme="d">
         原料
     </div>
-    <div data-role="controlgroup" data-type="horizontal" data-theme="d" id="materialSelection">
+    <div data-role="controlgroup" data-type="horizontal" data-theme="d" id="materialInPackagingSelection">
         <select id="materialInPackaging" name="material">
         <option>請選擇</option>
         </select>
     </div>
+    <div id="addedPackagingList"></div>
     <div data-role="controlgroup" data-type="horizontal" data-theme="d">
         包裝
         <input type="text" name="packaging" size=20 maxlength=16>
         單位重量
         <input type="text" name="unitWeight">
-        <input type="submit" value="新增" data-role="button">
+        <input type="submit" value="確定" data-role="button">
+        <input type="reset" value="新增" data-role="button">
     </div>
 </form>
 
